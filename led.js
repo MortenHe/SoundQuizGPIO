9b288eaae66955e853707ca3f1ec3338d8b1870c
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2018-07-06 16:31:02 +0200

    first commit

---
+++ led.js
@@ -0,0 +1,43 @@
+//Mit WebsocketServer verbinden
+const WebSocket = require('ws');
+const ws = new WebSocket('ws://localhost:8080');
+
+//GPIO Bibliothek laden
+const Gpio = require('onoff').Gpio;
+
+//LED 20 gruenes Licht fuer korrekte Antwort
+const ledGreen = new Gpio(20, 'out');
+
+//Wenn Verbindung mit WSS hergestellt wird
+ws.on('open', function open() {
+    console.log("connected to wss");
+
+    //Wenn WS eine Nachricht von WSS empfaengt
+    ws.on('message', function incoming(message) {
+
+        //Nachricht kommt als String -> in JSON Objekt konvertieren
+        var obj = JSON.parse(message);
+
+        //Wenn Auswertung der Frage geliefert wird
+        if (obj.type === "answer-state") {
+
+            //Wenn die Antwort richtig war
+            if (value === true) {
+                console.log("correct answer. Turn on green led");
+
+                //Gruene LED anschalten
+                ledGreen.writeSync(1);
+
+                //Nach 3 Sekunden wieder LED aus
+                setTimeout(() => {
+                    ledGreen.writeSync(0);
+                }, 3000);
+            }
+
+            //die Antwort war falsch
+            else {
+                console.log("wrong answer. Turn on red led");
+            }
+        }
+    });
+});
---
+++ rfid.js
@@ -0,0 +1,47 @@
+//Mit WebsocketServer verbinden
+console.log("connect to WS Server");
+const WebSocket = require('ws');
+const ws = new WebSocket('ws://localhost:8080');
+
+//RFID-Config-Datei lesen
+console.log("read rfid config file");
+const fs = require('fs-extra');
+const configObj = fs.readJsonSync('./config.json');
+
+//RFID Bibliothek laden
+var rc522 = require("rc522");
+
+//Wenn Verbindung mit WSS hergestellt wird
+ws.on('open', function open() {
+    console.log("connected to wss");
+
+    //Wenn ein RFID-Karte ausgelesen wurde
+    rc522(function (rfidSerialNumber) {
+        console.log("detected id: " + rfidSerialNumber);
+
+        //Wenn Karten-ID in Config vorkommt
+        if (configObj[rfidSerialNumber] !== undefined) {
+
+            //Karten-Daten auslesen aus Config
+            let cardData = configObj[rfidSerialNumber];
+
+            //MessageObjekt mit Info welche Audioplaylist / Video gespielt werden soll
+            let messageObj = {
+                type: cardData.type,
+                value: cardData.value
+            };
+            console.log("send card data: " + messageObj);
+
+            //Nachricht an WSS schicken
+            ws.send(JSON.stringify({
+                type: "send-card-data",
+                value: messageObj
+            }));
+        }
+
+        //Karte nicht in Config
+        else {
+            console.log("id not in config: " + rfidSerialNumber);
+        }
+    });
+});
